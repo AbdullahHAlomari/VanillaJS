e85a8288f0646e86b7b1b90ee3db81bbdd1546ff
Riyan <Riyan@DESKTOP-9Q695GJ>
2023-01-27 21:57:06 +0300

    Added formation button functionality - Clicking on a formation button now changes the pitch orientation

---
+++ Formation.js
@@ -0,0 +1,181 @@
+//6-3-1 gk top: 48%; left: 2%;
+
+
+
+
+function formation(i){
+
+    if(i===2){
+
+            document.getElementsByClassName("lb")[0].style.top='35%'
+            document.getElementsByClassName("lb")[0].style.left='14%'
+
+
+            document.getElementsByClassName("gk")[0].style.top='48%'
+            document.getElementsByClassName("gk")[0].style.left='2%'
+
+            document.getElementsByClassName("rb")[0].style.top='70%'
+            document.getElementsByClassName("rb")[0].style.left='14%'
+
+            document.getElementsByClassName("lwb")[0].style.top='23%'
+            document.getElementsByClassName("lwb")[0].style.left='14%'
+
+            document.getElementsByClassName("dm")[0].style.top='13%'
+            document.getElementsByClassName("dm")[0].style.left='25%'
+
+            document.getElementsByClassName("rwb")[0].style.top='87%'
+            document.getElementsByClassName("rwb")[0].style.left='25%'
+
+            document.getElementsByClassName("lm")[0].style.top='28%'
+            document.getElementsByClassName("lm")[0].style.left='38%'
+
+            document.getElementsByClassName("rm")[0].style.top='60%'
+            document.getElementsByClassName("rm")[0].style.left='39%'
+
+            document.getElementsByClassName("cm")[0].style.top='45%'
+            document.getElementsByClassName("cm")[0].style.left='30%'
+
+            document.getElementsByClassName("amr")[0].style.top='45%'
+            document.getElementsByClassName("amr")[0].style.left='60%'
+
+            document.getElementsByClassName("cb")[0].style.top='50%'
+            document.getElementsByClassName("cb")[0].style.left='14%'
+
+
+
+
+
+
+    }
+
+    if(i===1){
+
+            document.getElementsByClassName("lb")[0].style.top='48%'
+            document.getElementsByClassName("lb")[0].style.left='2%'
+
+
+            document.getElementsByClassName("gk")[0].style.top='80%'
+            document.getElementsByClassName("gk")[0].style.left='30%'
+
+            document.getElementsByClassName("rb")[0].style.top='20%'
+            document.getElementsByClassName("rb")[0].style.left='30%'
+
+            document.getElementsByClassName("lwb")[0].style.top='60%'
+            document.getElementsByClassName("lwb")[0].style.left='20%'
+
+            document.getElementsByClassName("dm")[0].style.top='35%'
+            document.getElementsByClassName("dm")[0].style.left='20%'
+            
+            document.getElementsByClassName("rwb")[0].style.top='80%'
+            document.getElementsByClassName("rwb")[0].style.left='60%'
+
+            document.getElementsByClassName("lm")[0].style.top='20%'
+            document.getElementsByClassName("lm")[0].style.left='60%'
+
+            document.getElementsByClassName("rm")[0].style.top='30%'
+            document.getElementsByClassName("rm")[0].style.left='50%'
+
+            document.getElementsByClassName("cm")[0].style.top='60%'
+            document.getElementsByClassName("cm")[0].style.left='50%'
+
+            document.getElementsByClassName("amr")[0].style.top='60%'
+            document.getElementsByClassName("amr")[0].style.left='83%'
+
+            document.getElementsByClassName("cb")[0].style.top='35%'
+            document.getElementsByClassName("cb")[0].style.left='83%'
+
+
+
+
+
+
+    }
+
+
+
+    if(i===0){
+
+        document.getElementsByClassName("lb")[0].style.top='48%'
+        document.getElementsByClassName("lb")[0].style.left='2%'
+
+
+        document.getElementsByClassName("gk")[0].style.top='70%'
+        document.getElementsByClassName("gk")[0].style.left='30%'
+
+        document.getElementsByClassName("rb")[0].style.top='20%'
+        document.getElementsByClassName("rb")[0].style.left='30%'
+
+        document.getElementsByClassName("lwb")[0].style.top='46%'
+        document.getElementsByClassName("lwb")[0].style.left='30%'
+
+        document.getElementsByClassName("dm")[0].style.top='46%'
+        document.getElementsByClassName("dm")[0].style.left='60%'
+        
+        document.getElementsByClassName("rwb")[0].style.top='80%'
+        document.getElementsByClassName("rwb")[0].style.left='70%'
+
+        document.getElementsByClassName("lm")[0].style.top='20%'
+        document.getElementsByClassName("lm")[0].style.left='70%'
+
+        document.getElementsByClassName("rm")[0].style.top='30%'
+        document.getElementsByClassName("rm")[0].style.left='50%'
+
+        document.getElementsByClassName("cm")[0].style.top='60%'
+        document.getElementsByClassName("cm")[0].style.left='50%'
+
+        document.getElementsByClassName("amr")[0].style.top='60%'
+        document.getElementsByClassName("amr")[0].style.left='83%'
+
+        document.getElementsByClassName("cb")[0].style.top='35%'
+        document.getElementsByClassName("cb")[0].style.left='83%'
+
+
+
+
+
+
+}
+
+if(i===5){
+
+    document.getElementsByClassName("lb")[0].style.top='0%'
+    document.getElementsByClassName("lb")[0].style.left='0%'
+
+    document.getElementsByClassName("gk")[0].style.top='0%'
+    document.getElementsByClassName("gk")[0].style.left='0%'
+
+    document.getElementsByClassName("rb")[0].style.top='0%'
+    document.getElementsByClassName("rb")[0].style.left='0%'
+
+    document.getElementsByClassName("lwb")[0].style.top='0%'
+    document.getElementsByClassName("lwb")[0].style.left='0%'
+
+    document.getElementsByClassName("dm")[0].style.top='0%'
+    document.getElementsByClassName("dm")[0].style.left='0%'
+    
+    document.getElementsByClassName("rwb")[0].style.top='0%'
+    document.getElementsByClassName("rwb")[0].style.left='0%'
+
+    document.getElementsByClassName("lm")[0].style.top='0%'
+    document.getElementsByClassName("lm")[0].style.left='0%'
+
+    document.getElementsByClassName("rm")[0].style.top='0%'
+    document.getElementsByClassName("rm")[0].style.left='0%'
+    document.getElementsByClassName("cm")[0].style.top='0%'
+    document.getElementsByClassName("cm")[0].style.left='0%'
+
+    document.getElementsByClassName("amr")[0].style.top='0%'
+    document.getElementsByClassName("amr")[0].style.left='0%'
+
+    document.getElementsByClassName("cb")[0].style.top='0%'
+    document.getElementsByClassName("cb")[0].style.left='0%'
+
+
+
+
+
+
+}
+
+}
+
